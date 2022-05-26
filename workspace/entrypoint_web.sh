9163dc19f3f5dfa54a7af4c591db220340e1fa9e
arm-band <bre4tn.let@gmail.com>
2022-05-26 10:51:41 +0900

    長いコマンドを改行

--- workspace/entrypoint_web.sh
+++ workspace/entrypoint_web.sh
@@ -2,15 +2,39 @@
 
 # gen key & certificate
 ## localhost
-openssl req -new -newkey rsa:2048 -nodes -out /etc/pki/tls/certs/localhost.csr -keyout /etc/pki/tls/private/localhost.key -subj "/C=/ST=/L=/O=/OU=/CN=www.example.com"
-openssl x509 -days 365 -req -signkey /etc/pki/tls/private/localhost.key -in /etc/pki/tls/certs/localhost.csr -out /etc/pki/tls/certs/localhost.crt
+openssl req -new -newkey rsa:2048 -nodes \
+        -out /etc/pki/tls/certs/localhost.csr \
+        -keyout /etc/pki/tls/private/localhost.key \
+        -subj "/C=/ST=/L=/O=/OU=/CN=www.example.com"
+openssl x509 -days 365 -req \
+        -signkey /etc/pki/tls/private/localhost.key \
+        -in /etc/pki/tls/certs/localhost.csr \
+        -out /etc/pki/tls/certs/localhost.crt
 ## .env domain
-openssl req -new -newkey rsa:2048 -nodes -out /etc/ssl/private/server.csr -keyout /etc/ssl/private/server.key -subj "/C=/ST=/L=/O=/OU=/CN=*.${2}"
-openssl x509 -days 365 -req -signkey /etc/ssl/private/server.key -in /etc/ssl/private/server.csr -out /etc/ssl/private/server.crt
+openssl req -new -newkey rsa:2048 -nodes \
+        -out /etc/ssl/private/server.csr \
+        -keyout /etc/ssl/private/server.key \
+        -subj "/C=/ST=/L=/O=/OU=/CN=*.${2}"
+openssl x509 -days 365 -req
+        -signkey /etc/ssl/private/server.key \
+        -in /etc/ssl/private/server.csr \
+        -out /etc/ssl/private/server.crt
 
 # setting file replace and copy
-sed -e "s/WEB_ROOT_DIRECTORY/${1}/gi" -e "s/WEB_DOMAIN/${2}/gi" -e "s/WEB_HOST_PORTNUM/${3}/gi" -e "s/WEB_CONTAINER_PORTNUM/${4}/gi" -e "s/WEB_HOST_PORTSSL/${5}/gi" -e "s/WEB_CONTAINER_PORTSSL/${6}/gi" /template/apache/apache_vh.conf > /etc/httpd/conf.d/${1}.conf
-sed -e "s/WEB_ROOT_DIRECTORY/${1}/gi" -e "s/WEB_DOMAIN/${2}/gi" -e "s/WEB_HOST_PORTNUM/${3}/gi" -e "s/WEB_CONTAINER_PORTNUM/${4}/gi" -e "s/WEB_HOST_PORTSSL/${5}/gi" -e "s/WEB_CONTAINER_PORTSSL/${6}/gi" /template/apache/apache_vh_ssl.conf > /etc/httpd/conf.d/${1}_ssl.conf
+sed -e "s/WEB_ROOT_DIRECTORY/${1}/gi" \
+    -e "s/WEB_DOMAIN/${2}/gi" \
+    -e "s/WEB_HOST_PORTNUM/${3}/gi" \
+    -e "s/WEB_CONTAINER_PORTNUM/${4}/gi" \
+    -e "s/WEB_HOST_PORTSSL/${5}/gi" \
+    -e "s/WEB_CONTAINER_PORTSSL/${6}/gi" \
+        /template/apache/apache_vh.conf > /etc/httpd/conf.d/${1}.conf
+sed -e "s/WEB_ROOT_DIRECTORY/${1}/gi" \
+    -e "s/WEB_DOMAIN/${2}/gi" \
+    -e "s/WEB_HOST_PORTNUM/${3}/gi" \
+    -e "s/WEB_CONTAINER_PORTNUM/${4}/gi" \
+    -e "s/WEB_HOST_PORTSSL/${5}/gi" \
+    -e "s/WEB_CONTAINER_PORTSSL/${6}/gi" \
+        /template/apache/apache_vh_ssl.conf > /etc/httpd/conf.d/${1}_ssl.conf
 
 cp /template/apache/php.conf /etc/httpd/conf.d/php.conf
 cp /template/apache/ssl.conf /etc/httpd/conf.d/ssl.conf
