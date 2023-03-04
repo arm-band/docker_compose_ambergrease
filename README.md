8bb443a873ee6eb98a68b85219a0af432cae02cf
arm-band <bre4tn.let@gmail.com>
2023-03-05 01:38:50 +0900

    - Composeファイル:     - `docker-compose.yml` を `compose.yml` に改名     - 永続化にデータ範囲を含めないようにComposeファイルを分離 - 共通:     - タイムゾーン変更を反映 - Webサーバ:     - SSHサーバ機能を追加     - FTPサーバとして vsftpd を追加     - PHP のバージョンを7.4と8.0で切り替えられるように改修 - コンテンツデータもすべて削除するリフレッシュのシェルスクリプトを用意 - PHPのログを削除するコマンドがなかったため追加

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ PHP + MySQL + phpMyAdmin の開発環境を Docker に構築する Docker Compos
 1. copy `sample.env` and rename `.env`
 2. change parameters in `.env`
 3. `docker-compose up -d`
+    - 仮想ホストとDBをファイル永続化する場合は `docker-compose -f compose.yml -f compose.volumes.yml up -d`
 4. `docker-compose exec web /bin/bash` or `docker-compose exec db /bin/bash` or `docker-compose exec phpmyadmin /bin/bash`
 
 ### Finish
--- workspace/entrypoint_web.sh
+++ workspace/entrypoint_web.sh
@@ -26,16 +26,40 @@ sed -e "s/WEB_ROOT_DIRECTORY/${1}/gi" \
     -e "s/WEB_CONTAINER_PORTSSL/${6}/gi" \
         /template/apache/apache_vh_ssl.conf > /etc/httpd/conf.d/${1}_ssl.conf
 
-cp /template/apache/php.conf /etc/httpd/conf.d/php.conf
+cp /template/apache/php-${12}.conf /etc/httpd/conf.d/php.conf
 cp /template/apache/ssl.conf /etc/httpd/conf.d/ssl.conf
 
 # Apache start
 /usr/sbin/httpd -DFOREGROUND &
 
+# SSH
+sed -ri 's/^#PermitRootLogin yes/PermitRootLogin yes/' /etc/ssh/sshd_config
+echo "${7}:${8}" | chpasswd
+ssh-keygen -t rsa -N "" -f /etc/ssh/ssh_host_rsa_key
+/usr/sbin/sshd -D &
+
 # WP CLI
-if [ "$7" = 'true' ]; then
+if [ "$9" = 'true' ]; then
     cd ~
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
     mv wp-cli.phar /usr/local/bin/wp
+    # permission
+    usermod -aG wheel,apache ${10}
+    chown -R apache:apache /var/www/${1}/web
+    find /var/www/${1}/web/ -type f -exec chmod 666 {} \;
+    find /var/www/${1}/web/ -type d -exec chmod 777 {} \;
 fi
+
+# FTP
+useradd ${10}
+echo ${11} | passwd --stdin ${10}
+
+cp /template/vsftpd/chroot_list /etc/vsftpd/chroot_list
+
+sed -e "s/WEB_FTP_USER/${10}/gi" \
+        /template/vsftpd/user_list > /etc/vsftpd/user_list
+sed -e "s/WEB_ROOT_DIRECTORY/${1}/gi" \
+        /template/vsftpd/WEB_FTP_USER > /etc/vsftpd/user_conf/${10}
+
+/usr/sbin/vsftpd &
